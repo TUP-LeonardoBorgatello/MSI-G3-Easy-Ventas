e517fd6d20eaff93ce962d7b550dd09e31f93a43
2w2111975LeonardoBorgatello <69583434+1w1111975LeonardoBorgatello@users.noreply.github.com>
2021-10-12 22:40:37 -0300

    Subiendo primer commit de MSI
    Proyecto TPI Easy Ventas. Primer push

---
+++ easyventas/src/main/java/com/msi/easyventas/TpimsiApplication.java
@@ -0,0 +1,13 @@
+package com.msi.easyventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpimsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpimsiApplication.class, args);
+	}
+
+}
