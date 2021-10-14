9ef42e5ee8346c9ecc26b1551c6739a632430f49
felipe-coggiola <yoarmus@gmail.com>
2021-10-13 22:49:38 -0300

    prueba1
    se sube configuracion

--- easyventas/src/main/java/com/msi/easyventas/TpimsiApplication.java
+++ easyventas/src/main/java/com/msi/easyventas/TpimsiApplication.java
@@ -2,8 +2,9 @@ package com.msi.easyventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
 public class TpimsiApplication {
 
 	public static void main(String[] args) {
