10a85d3c2d795eaaef95edd1f02a06fc2ef7950d
Lascirignola Paulo Martin <63617703+PauloL89@users.noreply.github.com>
2021-10-15 20:18:30 -0300

    Maven
    Se suben configuraciones

--- easyventas/src/main/java/com/msi/easyventas/TpimsiApplication.java
+++ easyventas/src/main/java/com/msi/easyventas/TpimsiApplication.java
@@ -2,9 +2,11 @@ package com.msi.easyventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class TpimsiApplication {
+public class TpimsiApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpimsiApplication.class, args);
