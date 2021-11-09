22dae5ef956b5ed70d591433afa3af18af25c8a1
Lascirignola Paulo Martin <63617703+PauloL89@users.noreply.github.com>
2021-11-09 20:40:23 -0300

    Mostrar Ciudades
    Se sube Método Mostrar ciudades

--- easyventas/src/main/java/com/msi/easyventas/services/AuxService.java
+++ easyventas/src/main/java/com/msi/easyventas/services/AuxService.java
@@ -1,6 +1,8 @@
 package com.msi.easyventas.services;
 
+import com.msi.easyventas.models.Ciudad;
 import com.msi.easyventas.models.TipoDoc;
+import com.msi.easyventas.repositories.CiudadRepository;
 import com.msi.easyventas.repositories.TipoDocRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,16 @@ public class AuxService {
 
     @Autowired
     TipoDocRepository tipoDocRepository;
+    @Autowired
+    CiudadRepository ciudadRepository;
 
     public List<TipoDoc> findAllTipoDoc() {
         List<TipoDoc> tipoDocs = tipoDocRepository.findAll();
         return tipoDocs;
+    }
+
+    public List<Ciudad> findAllCiudad() {
+        List<Ciudad> ciudades = ciudadRepository.findAll();
+        return ciudades;
     }
 }
