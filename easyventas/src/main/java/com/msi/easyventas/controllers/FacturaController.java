8fbfd3e1192c16a15d038d5f09525ce981e084ae
TUP-LeonardoBorgatello <111975@tecnicatura.frc.utn.edu.ar>
2021-11-12 02:43:56 -0300

    Refactor2

--- easyventas/src/main/java/com/msi/easyventas/controllers/FacturaController.java
+++ easyventas/src/main/java/com/msi/easyventas/controllers/FacturaController.java
@@ -4,6 +4,7 @@ import com.msi.easyventas.dtos.DetallePedidoResponseDTO;
 import com.msi.easyventas.dtos.FacturaRequestDTO;
 import com.msi.easyventas.dtos.FacturaResponseDTO;
 import com.msi.easyventas.services.FacturaService;
+import com.msi.easyventas.services.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,16 +31,17 @@ public class FacturaController {
 
     @PostMapping("/add/factura")
     public ResponseEntity<?> addFactura(@RequestBody FacturaRequestDTO nuevaFactura) throws Exception {
+        ServiceResponse<?> response = new ServiceResponse<>("success", "Factura agregado con éxito");
+        ServiceResponse<?> response2 = new ServiceResponse<>("error", "No se puedo agregar la factura. Verifique los datos.");
         if (nuevaFactura != null) {
             try {
                 facturaService.addFactura(nuevaFactura);
                 Thread.sleep(1000);
                 facturaService.addDetalleFactura(nuevaFactura);
             } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Alguno de los datos no existe. Verificar el" +
-                        " método de pago, la forma de entrega o si el pedido está cancelado.");
+                return new ResponseEntity<Object>(response2, HttpStatus.BAD_REQUEST);
             }
         }
-        return ResponseEntity.status(HttpStatus.OK).body("Factura cargada con éxito");
+        return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 }
