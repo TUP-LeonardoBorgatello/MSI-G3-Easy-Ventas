24ccee8dfa2df28b1f53a28f9b30c77c53b3ec70
TUP-LeonardoBorgatello <111975@tecnicatura.frc.utn.edu.ar>
2021-11-15 21:50:38 -0300

    Se sube Reporte q por parametro

--- easyventas/src/main/java/com/msi/easyventas/services/EmpleadoService.java
+++ easyventas/src/main/java/com/msi/easyventas/services/EmpleadoService.java
@@ -108,6 +108,8 @@ public class EmpleadoService implements iEmpleadoService {
                     if (empleado.getRolEmpleado().getId() == 2) {
                         Ciudad ciudad = ciudadRepository.findById(empleadoRequestDTO.getId_ciudad()).orElseThrow();
                         TipoDoc tipoDoc = tipoDocRepository.findById(empleadoRequestDTO.getId_tipo_doc()).orElseThrow();
+                        String contraseñaEmpleado;
+                        contraseñaEmpleado = empleado.getContraseña();
                         RolEmpleado rolEmpleado = new RolEmpleado();
                         rolEmpleado.setId(2);
 
@@ -120,6 +122,7 @@ public class EmpleadoService implements iEmpleadoService {
                         empleado.setNombre(empleadoRequestDTO.getNombre());
                         empleado.setDomicilio(empleadoRequestDTO.getDomicilio());
                         empleado.setTipoDoc(tipoDoc);
+                        empleado.setContraseña(contraseñaEmpleado);
                         empleadoRepository.save(empleado);
                     }
                     else{
